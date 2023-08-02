d46b9ef99532bd37872ea52c56adfc6b01082d26
tenntenn <uedatakuya@gmail.com>
2023-08-02 21:25:50 +0900

    add an inspect template

---
+++ inspect/inspect_test.go
@@ -0,0 +1,15 @@
+package inspect_test
+
+import (
+	"testing"
+
+	"github.com/gostaticanalysis/templates/inspect"
+	"github.com/gostaticanalysis/testutil"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
+	analysistest.Run(t, testdata, inspect.Analyzer, "a")
+}
---
+++ inspect/testdata/src/a/go.mod
@@ -0,0 +1,3 @@
+module github.com/gostaticanalysis/templates/inspect/testdata/src/a
+
+go 1.20
